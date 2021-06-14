30577ec9f85be162e639d20891957b4329a7d36e
Michael Haviv <mhaviv18@gmail.com>
2021-06-13 23:52:48 -0400

    Initial Commit

---
+++ MusiWeather/MusiWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusiWeatherApp.swift
+//  MusiWeather
+//
+//  Created by Michael Haviv on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MusiWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
