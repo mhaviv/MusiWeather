1a69fa97b5e8187418efe8fd2fcf7a3f9ecdf10f
Michael Haviv <mhaviv18@gmail.com>
2021-06-15 00:39:11 -0400

    Added text Field location icon, Added back button, refactored some of the code to different subviews, tweaked padding for logo

--- MusiWeather/Screens/CityFinderView.swift
+++ MusiWeather/Screens/CityFinderView.swift
@@ -9,35 +9,22 @@ import SwiftUI
 
 struct CityFinderView: View {
     
-    @State private var searchTerm = ""
-    
     var body: some View {
         ZStack {
             Image("musiweatherBGImage")
                 .resizable()
-            VStack {
-                Spacer()
-                Image("musiweatherLogo")
-                    .padding(.top, 20)
-//                Spacer()
-                Spacer(minLength: 748)
-            }
+            MWLogo()
             VStack {
                 Spacer()
                 HStack{
                     Spacer(minLength: 35)
-                    TextField("Search City", text: $searchTerm)
-                        .padding()
-                        .background(Color("textfieldBackground").cornerRadius(10))
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 1))
-//                        .border(Color.white)
-//                        .cornerRadius(10)
-                        .font(.custom("Avenir", size: 16).weight(.light))
-                        .foregroundColor(Color.white)
+                    CitySearchTextField()
+
                     Spacer(minLength: 35)
                 }
                 Spacer(minLength: 480)
             }
+            BackButton()
         }.edgesIgnoringSafeArea(.all)
     }
 }
@@ -45,5 +32,63 @@ struct CityFinderView: View {
 struct CityFinderView_Previews: PreviewProvider {
     static var previews: some View {
         CityFinderView()
+    }
+}
+
+struct CitySearchTextField: View {
+    
+    @State private var searchTerm = ""
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            if searchTerm.isEmpty { Text("Search City")
+                .foregroundColor(.white)
+                .font(.custom("Avenir", size: 16))
+                .fontWeight(.light)
+                .padding(.leading, 15)
+            }
+            HStack {
+                TextField("", text: $searchTerm)
+                    .font(.custom("Avenir", size: 16).weight(.light))
+                    .foregroundColor(Color.white)
+                Image(systemName: "location")
+                    .foregroundColor(Color("musiweatherOrange"))
+            }
+            .padding(12)
+            .background(Color("textfieldBackground").cornerRadius(10))
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 1))
+        }
+    }
+}
+
+struct BackButton: View {
+    var body: some View {
+        VStack {
+            Spacer(minLength: 750)
+            HStack{
+                HStack {
+                    Image(systemName: "chevron.backward")
+                        .font(.custom("SF Compact", size: 16).weight(.regular))
+                    Text("Back")
+                        .font(.custom("Avenir", size: 16).weight(.heavy))
+                }
+                .foregroundColor(Color.white)
+                .padding(.leading, 15)
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct MWLogo: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Image("musiweatherLogo")
+                .padding(.top, 100)
+            //                Spacer()
+            Spacer(minLength: 748)
+        }
     }
 }
